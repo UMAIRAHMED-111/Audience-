7c6effaa746797cdd27bc6bea7232bebe3f3dfb3
Umair Ahmed <umairahmed805@yahoo.com>
2022-12-28 16:43:46 +0500

    Adding Initial Project

---
+++ src/components/Footer.js
@@ -0,0 +1,39 @@
+import SocialIcons from './SocialIcons'
+import { Container } from './styles/Container.styled'
+import { Flex } from './styles/Flex.styled'
+import { StyledFooter } from './styles/Footer.styled'
+
+export default function Footer() {
+  return (
+    <StyledFooter>
+      <Container>
+
+        <Flex>
+          <ul>
+            <li>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua
+            </li>
+            <li>+92-123-4567</li>
+            <li>example@audience.com</li>
+          </ul>
+          <ul>
+            <li>About Us</li>
+            <li>What We Do</li>
+            <li>FAQ</li>
+          </ul>
+
+          <ul>
+            <li>Career</li>
+            <li>Blog</li>
+            <li>Contact Us</li>
+          </ul>
+
+          <SocialIcons />
+        </Flex>
+
+        <p>&copy; 2022 Audience++ - by Umair Ahmed. All rights reserved</p>
+      </Container>
+    </StyledFooter>
+  )
+}
